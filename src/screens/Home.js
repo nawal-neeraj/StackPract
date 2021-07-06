f5a2dd3bfe58b0f91270e1baf4d0b41288e0cddd
kumar.amit <kr.amit01@gmail.com>
2021-07-06 17:44:54 +0530

    dynamic value change

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,60 +1,75 @@
-import React from 'react';
-import { StyleSheet, View} from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Text, Root} from 'native-base';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput ,Dimensions} from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Text, Root, Input } from 'native-base';
 import Icon from "react-native-vector-icons/Ionicons";
-import  StyleConfig from "../Config/StyleConfig";
+import StyleConfig from "../Config/StyleConfig";
+const winds = Dimensions.get('window')
 
-export default Home = ({navigation}) => {
+export default Home = ({ navigation }) => {
+
+    const [title, setTitle] = useState("");
+    const [state, setState] = useState("")
 
     handlePage = () => {
         // console.log(navigation.na,"check")
-        navigation.navigate('Profile',{title:"Profile Form"})
+        navigation.navigate('Profile', { title: title })
     }
 
-    const MyStatusBar = ({backgroundColor, ...props}) => (
-        <View style={[styles.statusBar, { backgroundColor }]}>
-          <SafeAreaView>
-            <StatusBar translucent={true} backgroundColor={backgroundColor} {...props} />
-          </SafeAreaView>
-        </View>
-      );
+    const handleTitel = (value) => {
+        setTitle(value)
+        // console.log(value)
+    }
 
-  return (
-    <Container>
-        <StyleConfig />
-        <Header>
-          <Left>
-            <Button transparent> 
-                <Icon name='arrow-back-outline' size={20}/>
-            </Button>
-          </Left>
-          <Body>
-            <Title>
-              Home Page
-            </Title>
-          </Body>
-          <Right />
-        </Header>
-        <View style={styles.Cont}>
-            <View>
-          <Text style={{textAlign:'center'}}>
-            Hello This is Ios
-          </Text>
-            </View>
-          <View style={{padding:10}}>
-          <Button style={{alignSelf:'center'}} onPress={handlePage.bind(this)}>
-              <Text>Go To</Text>
-          </Button>
-          </View>
+    const MyStatusBar = ({ backgroundColor, ...props }) => (
+        <View style={[styles.statusBar, { backgroundColor }]}>
+            <SafeAreaView>
+                <StatusBar translucent={true} backgroundColor={backgroundColor} {...props} />
+            </SafeAreaView>
         </View>
-      </Container>
-  );
+    );
+
+    return (
+        <Container>
+            <StyleConfig />
+            <Header>
+                <Left>
+                    <Button transparent>
+                        <Icon name='arrow-back-outline' size={20} />
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>
+                        Home Page
+                    </Title>
+                </Body>
+                <Right />
+            </Header>
+            <View style={styles.Cont}>
+                <View style={{padding:10}}>
+                    <TextInput value={title} onChangeText={handleTitel} placeholder="Enter Title" />
+                </View>
+                {/* <View style={{padding:10}}>
+                    <Input value = {state} onChangeText={(e) => setState(e)} placeholder="Enter Title" />
+                </View> */}
+                <View style={{padding:10}}>
+                    <Text style={{ textAlign: 'center' }}>
+                        Hello This is Ios
+                    </Text>
+                </View>
+                <View >
+                    <Button style={{ alignSelf: 'center' }} onPress={handlePage.bind(this)}>
+                        <Text>Go To</Text>
+                    </Button>
+                </View>
+            </View>
+        </Container>
+    );
 }
 
 const styles = StyleSheet.create({
     Cont: {
-      justifyContent: 'center',
-      flex: 1,
-      alignItems:'center'
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center'
     }
-  })
+})
